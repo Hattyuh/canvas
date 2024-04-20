3537e2b35982c2cab5efa38f6290001dcda7ad38
Richard Kosa <165525466+Hattyuh@users.noreply.github.com>
2024-04-20 13:06:03 +0200

    uploading files

---
+++ script.js
@@ -0,0 +1,83 @@
+const canvas = document.getElementById("canvas");
+const context = canvas.getContext("2d");
+
+/*
+Task1 Chessboard
+
+const a = 50;
+const b = 50;
+const darkSquareColor = "#769656";
+const lightSquareColor = "#eeeed2";
+
+canvas.height = 9*a;
+canvas.width = 9*b;
+
+function drawSquare(x, y, color) {
+    context.fillStyle = color;
+    context.fillRect(x, y, a, b);
+}
+
+function drawRowA(startingX, startingY) {
+    for (let i = 0; i < 8; i++) {
+        if (i % 2 === 0) {
+            drawSquare(a*i+startingX,startingY,darkSquareColor);
+        } else {
+            drawSquare(a*i+startingX,startingY,lightSquareColor);
+        }
+    }
+}
+
+function drawRowB(startingX, startingY) {
+    for (let i = 0; i < 8; i++) {
+        if (i % 2 === 1) {
+            drawSquare(a*i+startingX,startingY,darkSquareColor);
+        } else {
+            drawSquare(a*i+startingX,startingY,lightSquareColor);
+        }
+    }
+}
+
+function drawChessboard(startingX, startingY) {
+    for (let i = 0; i < 8; i++) {
+        if (i % 2 === 1) {
+            drawRowA(startingX,a*i+startingY,darkSquareColor);
+        } else {
+            drawRowB(startingX,a*i+startingY,lightSquareColor);
+        }
+    } 
+}
+drawChessboard(a/2,b/2);
+*/
+
+
+//Task2 random circles
+canvas.height = window.innerHeight;
+canvas.width = window.innerWidth;
+
+
+const radius = 25;
+let x = 100;
+let y = 100;
+
+function randomColor() {
+    return "#" + Math.floor(Math.random()*16777215).toString(16);
+}
+
+function randomPosition(lastNum) {
+    return Math.floor(Math.random()*lastNum)
+}
+
+function drawCircle(x,y, color) {
+    context.beginPath();
+    context.strokeStyle = color;
+    context.arc(x,y,radius, 0, 2 * Math.PI), false;
+    context.stroke();
+}
+
+function randomCircles(numOfCircles) {
+    for (let i = 0; i < numOfCircles; i++) {
+        drawCircle(randomPosition(canvas.width), randomPosition(canvas.height), randomColor())
+    }
+}
+
+randomCircles(7000);
